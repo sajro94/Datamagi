d7d66acbbc59cc0cd638075e53bc5fa2e9d6ca0f
Flemming <sajro94@gmail.com>
2017-10-30 16:11:00 +0100

    Changes to yatzy 2

--- EclipseWS/Projekt2 Yatzy/src/src/yatzy/MainApp.java
+++ EclipseWS/Projekt2 Yatzy/src/src/yatzy/MainApp.java
@@ -1,16 +1,9 @@
 package src.yatzy;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -26,6 +19,7 @@ public class MainApp extends Application {
 		GridPane pane = new GridPane();
 		initContent(pane);
 		Scene scene = new Scene(pane);
+		scene.getStylesheets().add("stylesheet.css");
 		stage.setScene(scene);
 		stage.setResizable(false);
 		stage.show();
@@ -36,18 +30,32 @@ public class MainApp extends Application {
 	// The Yatzy game object
 	private Yatzy yatzy = new Yatzy();
 	// Shows the face values of the 5 dice.
-	private Button[] btnValues;
-	// Shows the hold status of the 5 dice.
-	private Label[] lblResults;
-	private TextField txfSumSame, txfBonus, txfSumOther, txfTotal;
-	private Label lblRolled;
+	private ToggleButton[] btnValues;
+	private String[] scoreNames = { "1'ere", "2'ere", "3'ere", "4'ere", "5'ere", "6'ere", "Et Par", "To Par", "Tre Ens",
+			"Fire Ens", "Fuldt Hus", "Lille Straight", "Stor Straight", "Chance", "Yatzy" };
+	private Button[] btnScores;
+	private Label[] lblScores;
+	private Label lblBonusName;
+	private Label lblTotalName;
+	private Label lblBonusScore;
+	private Label lblTotalScore;
 	private Button btnRoll;
+	private int[] results;
 
+	private int bonusCheck = 0;
+	private int totalScore = 0;
+
+	// Shows the hold status of the 5 dice.
 	private boolean[] holds;
+	private boolean[] scored;
 
 	private void initContent(GridPane pane) {
-		btnValues = new Button[5];
+		btnValues = new ToggleButton[5];
 		holds = new boolean[] { false, false, false, false, false };
+		scored = new boolean[15];
+		for (int i = 0; i < 15; i++) {
+			scored[i] = false;
+		}
 		pane.setGridLinesVisible(false);
 		pane.setPadding(new Insets(10));
 		pane.setHgap(10);
@@ -57,9 +65,10 @@ public class MainApp extends Application {
 
 		GridPane dicePane = new GridPane();
 		for (int i = 0; i < btnValues.length; i++) {
-			btnValues[i] = new Button("-");
+			btnValues[i] = new ToggleButton("-");
 			btnValues[i].setPrefSize(40, 40);
 			btnValues[i].setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 16));
+			btnValues[i].setDisable(true);
 			int index = i;
 			btnValues[i].setOnAction(event -> valueButtonAction(index));
 			dicePane.add(btnValues[i], i, 0);
@@ -82,7 +91,36 @@ public class MainApp extends Application {
 
 		// ---------------------------------------------------------------------
 
+		btnScores = new Button[15];
+		lblScores = new Label[15];
+
 		GridPane scorePane = new GridPane();
+		for (int i = 0; i < 15; i++) {
+			int index = i;
+			lblScores[i] = new Label(scoreNames[i]);
+			btnScores[i] = new Button("0");
+			btnScores[i].setPrefSize(40, 25);
+			btnScores[i].setId("open");
+			btnScores[i].setDisable(true);
+			btnScores[i].setOnAction(event -> scoreResultAction(index));
+			if (i > 5) {
+				scorePane.add(lblScores[i], 0, i + 1);
+				scorePane.add(btnScores[i], 1, i + 1);
+
+			} else {
+				scorePane.add(lblScores[i], 0, i);
+				scorePane.add(btnScores[i], 1, i);
+			}
+		}
+		lblBonusName = new Label("Bonus");
+		lblBonusScore = new Label("0");
+		lblTotalName = new Label("Total");
+		lblTotalScore = new Label("0");
+		lblTotalScore.setPrefSize(40, 25);
+		scorePane.add(lblBonusName, 0, 6);
+		scorePane.add(lblBonusScore, 1, 6);
+		scorePane.add(lblTotalName, 0, 16);
+		scorePane.add(lblTotalScore, 1, 16);
 		pane.add(scorePane, 0, 1);
 		scorePane.setGridLinesVisible(false);
 		scorePane.setPadding(new Insets(10));
@@ -94,10 +132,52 @@ public class MainApp extends Application {
 		// labels and text fields for sums, bonus and total.
 	}
 
+	private boolean givenBonus = false;
+
+	private void scoreResultAction(int index) {
+		if (index < 6) {
+			bonusCheck += results[index];
+			if (!givenBonus && bonusCheck >= 63) {
+				givenBonus = true;
+				totalScore += 50;
+				lblBonusScore.setText("50");
+			}
+		}
+		totalScore += results[index];
+		lblTotalScore.setText("" + totalScore);
+		scored[index] = true;
+		toggleScoreButtons(true);
+		toggleRollButton();
+		btnScores[index].setDisable(true);
+		btnScores[index].setId("scored");
+		if (allScored()) {
+			btnRoll.setText("Reset Game");
+			btnRoll.setOnAction(event -> resetGameAction());
+		}
+	}
+
+	private void resetGameAction() {
+		for (int i = 0; i < 15; i++) {
+			btnScores[i].setText("0");
+			btnScores[i].setId("open");
+			scored[i] = false;
+		}
+		lblTotalScore.setText("0");
+		lblBonusScore.setText("0");
+		totalScore = 0;
+		bonusCheck = 0;
+		givenBonus = false;
+
+		btnRoll.setOnAction(event -> rollDices());
+		toggleRollButton();
+		toggleDieButtons(true);
+		toggleScoreButtons(true);
+	}
+
 	private void rollDices() {
 		yatzy.throwDice(holds);
+		toggleDieButtons(false);
 		int[] values = yatzy.getValues();
-		Random r = new Random();
 
 		for (int i = 0; i < values.length; i++) {
 			btnValues[i].setText(values[i] + "");
@@ -105,36 +185,62 @@ public class MainApp extends Application {
 
 		btnRoll.setText(String.format("Roll(%s)", 3 - yatzy.getThrowCount()));
 		if (yatzy.getThrowCount() == 3) {
+			toggleDieButtons(true);
 			btnRoll.setDisable(true);
+			toggleScoreButtons(false);
+			showResults();
 		}
 	}
 
-	private void localWait(long milis) {
-		long start = System.currentTimeMillis();
-		while (System.currentTimeMillis() - start < milis) {
-
+	private void showResults() {
+		results = yatzy.getPossibleResults();
+		for (int i = 0; i < btnScores.length; i++) {
+			if (!scored[i]) {
+				btnScores[i].setText("" + results[i]);
+			}
 		}
+	}
+
+	private void toggleRollButton() {
+		yatzy.resetThrowCount();
+		btnRoll.setText("Roll(3)");
+		btnRoll.setDisable(false);
+	}
+
+	private void toggleScoreButtons(boolean b) {
+		for (int i = 0; i < btnScores.length; i++) {
+			if (!scored[i]) {
+				btnScores[i].setDisable(b);
+			}
+		}
+	}
+
+	private void toggleDieButtons(boolean b) {
+		for (int i = 0; i < btnValues.length; i++) {
+			btnValues[i].setDisable(b);
+			if (b) {
+				btnValues[i].setSelected(false);
+				holds[i] = false;
+			}
+		}
+
 	}
 
 	private void valueButtonAction(int index) {
 		if (holds[index]) {
-			btnValues[index].setStyle("");
 			holds[index] = false;
 		} else {
-			btnValues[index].setStyle("-fx-background-color: darkGrey;");
 			holds[index] = true;
 		}
 	}
 
-	// -------------------------------------------------------------------------
-
-	// TODO: Create a method for btnRoll's action.
-	// Hint: Create small helper methods to be used in the action method.
-
-	// -------------------------------------------------------------------------
-
-	// TODO: Create a method for mouse click on one of the text fields in
-	// txfResults.
-	// Hint: Create small helper methods to be used in the mouse click method.
+	private boolean allScored() {
+		for (boolean b : scored) {
+			if (!b) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
